bfa12e56323f8b9e80f1353ed87172a7c8a5784d
Yujie <yp25@st-andrews.ac.uk>
2024-05-09 16:41:47 +0100

    build the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar"
+import { cn } from "@/lib/utils"
+import "@/styles/globals.css"
+import { Inter } from "next/font/google"
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
 }
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
@@ -11,8 +16,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "text-slate-900 bg-white light antialiased light",
+        inter.className
+      )}
+    >
+      <body
+        className={cn(
+          "min-h-screen bg-slate-50 pt-12  antialiased ",
+          inter.className
+        )}
+      >
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
