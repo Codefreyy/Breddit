11f06447ccbf8b91766ea7933118e607e29a28c5
Yujie <yp25@st-andrews.ac.uk>
2024-05-28 22:40:54 +0100

    fix build error

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,28 +1,24 @@
 import { db } from "@/lib/db"
 
 export async function GET(req: Request) {
-    try {
-        // get the query params
-        const url = new URL(req.url)
-        const q = url.searchParams.get('q')
+    // get the query params
+    const url = new URL(req.url)
+    const q = url.searchParams.get('q')
 
-        if (!q) return new Response('No query provided', { status: 400 })
-        const res = await db.subreddit.findMany({
-            where: {
-                name: {
-                    contains: q
-                }
-            },
-            include: {
-                _count: true
-            },
-            take: 5
-        })
+    if (!q) return new Response('No query provided', { status: 400 })
+    const res = await db.subreddit.findMany({
+        where: {
+            name: {
+                contains: q
+            }
+        },
+        include: {
+            _count: true
+        },
+        take: 5
+    })
 
-        return new Response(JSON.stringify(res), { status: 200 })
+    return new Response(JSON.stringify(res), { status: 200 })
 
 
-    } catch (error) {
-
-    }
 }
--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -53,7 +53,7 @@ const Layout = async ({
   return (
     <div className="sm:container h-fullo max-w-7xl mx-auto">
       <div>
-        {/* back to feed button */}
+        {/* Todo: back to feed button */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <ul className="flex flex-col col-span-2 space-y-6">{children}</ul>
           <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
