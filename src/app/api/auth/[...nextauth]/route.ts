38290fc16321559eddb388156981c34c5ac56cb3
Yujie <yp25@st-andrews.ac.uk>
2024-05-30 23:18:35 +0100

    delete maxDuration in auth/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import { authOptions } from '@/lib/auth'
 import NextAuth from 'next-auth/next'
 
-export const maxDuration = 300
-
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
